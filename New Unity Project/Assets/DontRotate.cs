6bd334d881454f4658f8f4e5c9e2422b12b79446
lee <q2298478324@gmail.com>
2019-04-16 22:52:56 +0900

    2を追懐

---
+++ New Unity Project/Assets/DontRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontRotate : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = new Quaternion(0,0,0,1);       
+    }
+}
